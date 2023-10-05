0b38a862a3494535abae2c4d6828e5a3869339e5
Payam Zahedi <pzahedi1995@gmail.com>
2023-10-05 18:17:32 +0330

    initial commit

---
+++ bin/raspberry_device.dart
@@ -0,0 +1,5 @@
+import 'package:raspberry_device/raspberry_device.dart' as raspberry_device;
+
+void main(List<String> arguments) {
+  print('Hello world: ${raspberry_device.calculate()}!');
+}
---
+++ test/raspberry_device_test.dart
@@ -0,0 +1,8 @@
+import 'package:raspberry_device/raspberry_device.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
