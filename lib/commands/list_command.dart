9a70534881dcb7a7770f8ada84530172d67d103c
Payam Zahedi <pzahedi1995@gmail.com>
2023-10-23 12:45:00 +0330

    add base command

--- lib/commands/list_command.dart
+++ lib/commands/list_command.dart
@@ -5,6 +5,8 @@ import 'package:raspberry_device/utils/flutter_sdk.dart';
 import 'package:flutter_tools/src/context_runner.dart';
 import 'package:process_run/process_run.dart' as process_run;
 
+/// List all custom devices added to the Flutter SDK with custom-devices command
+/// it will utilize the `flutter custom-devices list` command to show the list
 class ListCommand extends BaseCommand {
   final sdkManager = FlutterSdkManager();
 
