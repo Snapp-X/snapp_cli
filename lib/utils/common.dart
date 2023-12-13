4984305c65d218e15985d16054fc8aae3afb72b0
Payam Zahedi <pzahedi1995@gmail.com>
2023-12-13 11:42:57 +0100

    add extension method to the ProcessRunner

--- lib/utils/common.dart
+++ lib/utils/common.dart
@@ -1,13 +1,16 @@
 // ignore_for_file: implementation_imports
 
+import 'dart:convert';
 import 'dart:io';
 import 'package:flutter_tools/src/base/logger.dart';
 import 'package:interact/interact.dart';
 
 import 'package:collection/collection.dart';
 import 'package:flutter_tools/src/base/io.dart';
+import 'package:flutter_tools/src/base/process.dart';
 
 import 'package:flutter_tools/src/custom_devices/custom_device_config.dart';
+import 'package:process/process.dart';
 
 final RegExp hostnameRegex = RegExp(
     r'^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$');
@@ -76,5 +79,72 @@ extension CustomDevicesConfigExt on CustomDeviceConfig {
   String get deviceUsername {
     // TODO(payam): Complete this method
     throw UnimplementedError();
+  }
+}
+
+extension ProcessUtilsExt on ProcessUtils {
+  Future<RunResult> runWithOutput(
+    List<String> cmd, {
+    required ProcessManager processManager,
+    required Logger logger,
+    Duration? timeout,
+  }) async {
+    while (true) {
+      final Process process = await start(cmd);
+
+      final StringBuffer stdoutBuffer = StringBuffer();
+      final StringBuffer stderrBuffer = StringBuffer();
+      final Future<void> stdoutFuture =
+          process.stdout.transform<String>(const Utf8Decoder()).listen(
+        (event) {
+          stdoutBuffer.write(event);
+          logger.printStatus(event);
+        },
+      ).asFuture<void>();
+      final Future<void> stderrFuture = process.stderr
+          .transform<String>(const Utf8Decoder())
+          .listen(stderrBuffer.write)
+          .asFuture<void>();
+
+      int? exitCode;
+      exitCode = timeout == null
+          ? await process.exitCode.then<int?>((int x) => x)
+          : await process.exitCode.then<int?>((int x) => x).timeout(timeout,
+              onTimeout: () {
+              // The process timed out. Kill it.
+              processManager.killPid(process.pid);
+              return null;
+            });
+
+      String stdoutString;
+      String stderrString;
+      try {
+        Future<void> stdioFuture =
+            Future.wait<void>(<Future<void>>[stdoutFuture, stderrFuture]);
+        if (exitCode == null) {
+          // If we had to kill the process for a timeout, only wait a short time
+          // for the stdio streams to drain in case killing the process didn't
+          // work.
+          stdioFuture = stdioFuture.timeout(const Duration(seconds: 1));
+        }
+        await stdioFuture;
+      } on Exception {
+        // Ignore errors on the process' stdout and stderr streams. Just capture
+        // whatever we got, and use the exit code
+      }
+      stdoutString = stdoutBuffer.toString();
+      stderrString = stderrBuffer.toString();
+
+      final ProcessResult result = ProcessResult(
+          process.pid, exitCode ?? -1, stdoutString, stderrString);
+      final RunResult runResult = RunResult(result, cmd);
+
+      // If the process did not timeout. We are done.
+      if (exitCode != null) {
+        logger.printTrace(runResult.toString());
+
+        return runResult;
+      }
+    }
   }
 }
